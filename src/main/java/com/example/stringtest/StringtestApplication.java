69d14bf03c275ef92b6939eb9f433ed72ee954f2
sneha-paul <sneha.paul@e-xposures.com>
2021-11-26 23:40:29 +0530

    string code change4

--- src/main/java/com/example/stringtest/StringtestApplication.java
+++ src/main/java/com/example/stringtest/StringtestApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StringtestApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(StringtestApplication.class, args);
 	}
