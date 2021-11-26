466793dc48d52288948778a9b9d96a23e3b01e4a
sneha-paul <sneha.paul@e-xposures.com>
2021-11-27 00:53:23 +0530

    stringTEST code change11

--- src/test/java/com/example/stringtest/StringUtilTest.java
+++ src/test/java/com/example/stringtest/StringUtilTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertTrue;
 public class StringUtilTest {
 
     @Test
-
     public void testIsEmpty() {
         assertTrue(StringUtil.isEmpty(""));
         assertTrue(StringUtil.isEmpty(null));
